7a61d50d42e5c341d3a0e508f1985244d9d61fe2
gabrielgoncalfreitas <gabriel.goncal.freitas@gmail.com>
2024-11-26 23:51:00 -0300

    Atualiza o projeto

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-fetch(
-  "http://145.223.31.50:3000/api/box/deploy/05354d7b5529b97c20332469b5138799647392de2a9e0ea0",
-  {
-    method: "POST",
-  }
-);
+const express = require("express");
+
+const app = express();
+
+app.post("/", () => {
+  fetch(
+    "http://145.223.31.50:3000/api/box/deploy/05354d7b5529b97c20332469b5138799647392de2a9e0ea0",
+    {
+      method: "POST",
+    }
+  );
+});
+
+app.listen(30000);
