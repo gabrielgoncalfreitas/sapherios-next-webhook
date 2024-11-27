6f0cb29b175ce3376c6db42ae7a7ff62667ba904
gabrielgoncalfreitas <gabriel.goncal.freitas@gmail.com>
2024-11-26 23:56:23 -0300

    Atualiza a rota de webhook

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.post("/", () => {
+app.post("/webhook", () => {
   fetch(
     "http://145.223.31.50:3000/api/box/deploy/05354d7b5529b97c20332469b5138799647392de2a9e0ea0",
     {
