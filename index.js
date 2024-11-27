a92cf95b488160390593554888ff5ac09417d1bb
gabrielgoncalfreitas <gabriel.goncal.freitas@gmail.com>
2024-11-27 00:08:40 -0300

    Atualiza o projeto

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.send("pong");
+app.post("/webhook", async (req, res) => {
+  try {
+    const easypanelUrl = "http://145.223.31.50:3000/api/box/deploy/05354d7b5529b97c20332469b5138799647392de2a9e0ea0";
+    const params = new URLSearchParams(req.body).toString(); // Converte o body do POST para query params
+
+    await axios.get(`${easypanelUrl}?${params}`);
+    res.status(200).send("Requisição redirecionada com sucesso!");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erro ao redirecionar a requisição.");
+  }
 });
 
-app.post("/webhook", () => {
-  fetch(
-    "http://145.223.31.50:3000/api/box/deploy/05354d7b5529b97c20332469b5138799647392de2a9e0ea0",
-    {
-      method: "POST",
-    }
-  );
-});
-
-app.listen(3001);
+app.listen(3000, () => console.log("Middleware rodando na porta 3000"));
