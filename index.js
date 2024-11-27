6e60218c90ebd6493223186c153804bc57f8a1dd
gabrielgoncalfreitas <gabriel.goncal.freitas@gmail.com>
2024-11-27 00:01:57 -0300

    Atualiza

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  return res.send("pong");
+});
+
 app.post("/webhook", () => {
   fetch(
     "http://145.223.31.50:3000/api/box/deploy/05354d7b5529b97c20332469b5138799647392de2a9e0ea0",
@@ -11,4 +15,4 @@ app.post("/webhook", () => {
   );
 });
 
-app.listen(30000);
+app.listen(3001);
